1f174a04be4af47b6c92daa405130e547e48495c
Muhammad Waqas Azeem <160093061+Muhammad-Waqas-Azeem@users.noreply.github.com>
2024-04-22 12:42:56 +0500

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-export {};
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        type: "input",
+        name: "paragrph",
+        message: "Enter your paragraph or text"
+    }
+]);
+console.log("\n");
+let character = answer.paragrph.split(" ").join('');
+const words = answer.paragrph.trim().split(" ");
+console.log(`Total words in your paragraph is ${words.length} & total alphabets in your paragraph is ${character.length}`);
--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-import inquirer from "inquirer";
+#! /usr/bin/env node
+import inquirer from "inquirer";  
+const answer = await inquirer.prompt([
+    {
+        type: "input",
+        name: "paragrph",
+       message: "Enter your paragraph or text" 
+    }])
+    console.log("\n");
+    let character=answer.paragrph.split(" ").join('');
+    const words=answer.paragrph.trim().split(" ");
+    console.log(`Total words in your paragraph is ${words.length} & total alphabets in your paragraph is ${character.length}`);
